119c42339faa39e51a843e9c643b838868aecbb8
Swapnil Shende <swapnilshende1998@gmail.com>
2024-07-11 15:49:43 +0530

    add two number program

---
+++ JavaScript Basics Program/01 AddTwoNumber.js
@@ -0,0 +1,24 @@
+// Using + Operator
+
+let num1 = 10;
+let num2 = 10;
+console.log(num1 + num2);
+
+// Using function
+
+function addNumber(a, b) {
+  return a + b;
+}
+console.log(addNumber(50, 25));
+
+// Using Arrow function
+
+const addnumbers = (a, b) => console.log(a + b);
+addnumbers(50, 50);
+
+// Using Addition Assignment (+=) Operator
+
+let A = 10;
+let B = 15;
+A += B;
+console.log(A);
